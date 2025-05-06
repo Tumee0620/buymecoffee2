2c3501877b4c0468c19b08ecc7fae81ca88af418
Tumee tumee <ttumee0620@gmail.com>
2025-05-06 12:32:23 +0800

    component test

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Division } from "./components/Division";
+
 export default function Home() {
-  return <div>home world</div>;
+  return (
+    <div>
+      <Division />
+    </div>
+  );
 }
