63dce2ded47565ce1f8ffc4a78c05752c3d87d57
Tumee tumee <ttumee0620@gmail.com>
2025-05-09 09:35:22 +0800

    backendsetup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,11 @@
-import express from "express";
+import express, { json } from "express";
+import { userRouter } from "./routers/user";
+import { createUser } from "./controllers/user";
 
 const app = express();
 
+app.use(json());
+app.use("/user", userRouter);
 app.get("/", (req, res) => {
   res.send("hello world");
 });
