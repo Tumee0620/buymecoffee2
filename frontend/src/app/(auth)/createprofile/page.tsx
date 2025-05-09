12eeffad684143c64846ccab48df1e9770a5deeb
Tumee tumee <ttumee0620@gmail.com>
2025-05-09 11:32:21 +0800

    createprofileand

---
+++ frontend/src/app/(auth)/createprofile/page.tsx
@@ -0,0 +1,13 @@
+import { FiCoffee } from "react-icons/fi";
+
+const ProfileCreate = () => {
+  <div className="bg-white w-full h-full">
+    <div>
+      <div className="flex gap-2 text-black font-bold pl-10  items-center">
+        <FiCoffee color="black" />
+        <p>Buy Me Coffee</p>
+      </div>
+    </div>
+  </div>;
+};
+export default ProfileCreate;
